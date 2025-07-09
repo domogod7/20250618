06349effae71839f13b3eb377d9768de59ac70e5
domogod7 <daejung3609@dge.go.kr>
2025-07-09 16:09:51 +0900

    0

--- njs2.js
+++ njs2.js
@@ -1,3 +1,3 @@
 document.write("njs2.js는   njs1.js에   포함");
 document.write("<div style='color: blue; font-size: 20px;'>외부 자바스크립트 파일</div>");
-document.write("<script src='./ejs/njs3.js'> </script>");
+document.write("<script src='./njs3.js'> </script>");
